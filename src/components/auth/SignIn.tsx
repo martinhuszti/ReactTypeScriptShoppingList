3b3b88eb58300d774029e58dae35beb71fd1f15c
Martin <martinhuszti@gmail.com>
2019-05-31 23:34:58 +0200

    Login css

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { connect } from 'react-redux';
 import { signIn } from '../../store/actions/authActions';
 import { Redirect } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap'
 
 const SignIn = (props: any) => {
 
@@ -15,25 +16,32 @@ const SignIn = (props: any) => {
     if (auth.uid) return <Redirect to='/' />
 
     return (
-            <div className="container">
-                <form onSubmit={handleSubmit} className="white">
-                    <h5 className="grey-text text-darken-3">Sign In</h5>
-                    <div className="input-field">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" autoComplete="email" id="email" onChange={e => cred.email = (e.target.value)} />
-                    </div>
-                    <div className="input-field">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" autoComplete="current-password" id="password" onChange={e => cred.password = (e.target.value)} />
-                    </div>
-                    <div className="input-field">
-                        <button className="btn pink lighten-1 ">Login</button>
-                    </div>
-                    <div className="red-text center">
-                        {authError ? <p>{authError}</p> : null}
-                    </div>
-                </form>
+        <div className="container">
+
+
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email cím</Form.Label>
+                    <Form.Control onChange={(e: any) => cred.email = (e.target.value)} type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+  </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Jelszó</Form.Label>
+                    <Form.Control onChange={(e: any) => cred.password = (e.target.value)} type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Bejelentkezés
+</Button>
+            </Form>
+            
+            <div className="red-text center">
+                {authError ? <p>{authError}</p> : null}
             </div>
+        </div>
+
     )
 }
 
--- src/components/shoppingitem/ItemSummary.tsx
+++ src/components/shoppingitem/ItemSummary.tsx
@@ -8,7 +8,7 @@ const styles = require('./ItemSummaryStyle.css');
 
 const ItemSummary = ({ item }: { item: ShoppingItem }) => {
     return (
-        <Card className="shopping item card" bg="info" text="white" style={{ width: '18rem' }}>
+        <Card className="shopping item card" bg="warning" text="white" style={{ width: '18rem' }}>
         <Card.Header>{item.title}</Card.Header>
         <Card.Body>
           <Card.Title>Felvette:{item.createdBy}</Card.Title>
